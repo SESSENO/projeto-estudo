1dd15eb986eca62c11de77298ac6c5c747e83801
Sesseno <sfsena@gmail.com>
2022-01-07 15:59:32 -0300

    Atualizações aula 7Jan2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  nome = 'Sérgio Sena';
+  idade = 51;
   title = 'projeto-estudo';
+  alturaImagem=30;
+  larguraImagem=30;
+
+  linkImagem ='https://ciclovivo.com.br/wp-content/uploads/2018/10/iStock-536613027.jpg';
+
+  calculaAnoNascimento(): number {
+    const today = new Date();
+
+    return today.getFullYear() - this.idade;
+  }
+
+  aumentar() {
+    this.larguraImagem += 20;
+    this.alturaImagem += 15;
+  }
+
+  diminuir() {
+    this.larguraImagem -= 20;
+    this.alturaImagem -= 15;
+  }
+
 }
---
+++ src/app/exercicio-um/exercicio-um.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicio-um',
+  templateUrl: './exercicio-um.component.html',
+  styleUrls: ['./exercicio-um.component.scss']
+})
+export class ExercicioUmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
+// Crie uma propriedade que é um array de links de imagens.
+// Crie um botão para trocar a imagem para a próxima no array.
